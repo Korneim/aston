3a5d8c645ff11bed7339d860abfe623f0685e456
Mike Kornishin <mrkruner1@gmail.com>
2025-07-01 18:27:39 +0300

    feat: create basic logic for ThemeContext

---
+++ news-api/src/shared/lib/theme/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import { type FC, type ReactNode, useCallback, useState } from 'react';
+import { ThemeContext } from './useTheme';
+
+type Props = {
+    children: ReactNode;
+};
+
+export const ThemeProvider: FC<Props> = ({ children }) => {
+    const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+    const toggleTheme = useCallback(() => {
+        if (theme === 'light') {
+            setTheme('dark');
+        }
+        setTheme('light');
+    }, [theme]);
+
+    return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
