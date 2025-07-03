06af53bc46f31ef2f366839a737d7626ebfe66e2
Mike Kornishin <mrkruner1@gmail.com>
2025-07-03 14:46:45 +0300

    refactor: replace header, footer

--- news-api/src/app/App.tsx
+++ news-api/src/app/App.tsx
@@ -1,11 +1,14 @@
 import { MainLayout, ThemeProvider } from '../shared';
 import { PostList } from '../widgets/PostList';
+import { Footer, Header } from '../widgets';
 
 function App() {
     return (
         <ThemeProvider>
             <MainLayout>
+                <Header />
                 <PostList />
+                <Footer />
             </MainLayout>
         </ThemeProvider>
     );
--- news-api/src/shared/layouts/MainLayout.tsx
+++ news-api/src/shared/layouts/MainLayout.tsx
@@ -1,21 +1,22 @@
 import type { FC, ReactNode } from 'react';
-import { Footer, Header } from '../../widgets';
 import { useTheme } from '../lib';
 
 type Props = {
+    header?: ReactNode;
     children: ReactNode;
+    footer?: ReactNode;
 };
 
-export const MainLayout: FC<Props> = ({ children }) => {
+export const MainLayout: FC<Props> = ({ children, header, footer }) => {
     const { theme } = useTheme();
 
     return (
         <div
             style={{ background: theme === 'light' ? 'white' : 'black', color: theme === 'light' ? 'black' : 'white' }}
         >
-            <Header />
+            {header}
             {children}
-            <Footer />
+            {footer}
         </div>
     );
 };
