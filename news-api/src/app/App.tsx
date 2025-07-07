00bec4c9981ec92543ccf77eeb750aa85b0b5cea
Mike Kornishin <mrkruner1@gmail.com>
2025-07-07 17:17:18 +0300

    feat: create a HOC

--- news-api/src/app/App.tsx
+++ news-api/src/app/App.tsx
@@ -1,13 +1,20 @@
 import { MainLayout, ThemeProvider } from '../shared';
 import { PostList } from '../widgets/PostList';
 import { Footer, Header } from '../widgets';
+import { useState } from 'react';
+import { withLoading } from '../shared/lib/hoc/withLoading.tsx';
 
 function App() {
+    const [isLoading, setLoading] = useState(true);
+
+    setTimeout(setLoading, 1000, false);
+    const PostListWithLoading = withLoading(PostList);
+
     return (
         <ThemeProvider>
             <MainLayout>
                 <Header />
-                <PostList />
+                <PostListWithLoading isLoading={isLoading} />
                 <Footer />
             </MainLayout>
         </ThemeProvider>
